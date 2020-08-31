f6a2864f234fd8d8f5bdf1ee2b97400f8c52fc66
pl9ed <pl9ed@virginia.edu>
2020-08-30 23:26:00 -0400

    Added FM services

--- src/main/java/com/revature/services/FMServices.java
+++ src/main/java/com/revature/services/FMServices.java
@@ -1,41 +1,156 @@
 package com.revature.services;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.HashSet;
+import java.time.LocalDate;
 import java.util.Set;
+import java.util.TreeSet;
 
 import org.apache.log4j.Logger;
 
 import com.revature.DAO.ReimbDAO;
 import com.revature.data.Reimbursement;
+import com.revature.data.User;
+import com.revature.exceptions.SQLSecurityException;
 
 public class FMServices {
+	private int user;
 	private ReimbDAO dao;
 	private static Logger devlog = Logger.getLogger(FMServices.class);
-	private static Logger infolog = Logger.getLogger("infoLog");
-	
-	public FMServices() {
+
+	public FMServices(int user) {
+		this.user = user;
 		dao = new ReimbDAO();
 	}
-	
-	public FMServices(ReimbDAO dao) {
+
+	public FMServices(int user, ReimbDAO dao) {
+		this.user = user;
 		this.dao = dao;
 	}
-	
+
 	public boolean approveReimb(int id) {
 		return dao.processReimbursement(id, true);
 	}
-	
+
 	public boolean denyReimb(int id) {
 		return dao.processReimbursement(id, false);
 	}
-	
-	public Set<Reimbursement> filterBy(String s) {
-		// TODO implement
-		Set<Reimbursement> ret = new HashSet<Reimbursement>(); 
+
+	/**
+	 * 
+	 * @param status 1: approved, 0:pending, -1: denied
+	 * @return Set of reimbursements for the specified filter
+	 */
+	public Set<Reimbursement> findByStatus(int status) {
+		Set<Reimbursement> ret = new TreeSet<Reimbursement>();
+		try {
+			ret = dao.filterByIntField("STATUS_ID", status);
+		} catch (SQLSecurityException e) {
+			devlog.trace(this, e);
+		}
 		return ret;
 	}
+
+	public Set<Reimbursement> getAllReimbursements() {
+		return dao.getAllReimbursements();
+	}
+
+	public Set<Reimbursement> amountGreaterThan(double amt) {
+		Set<Reimbursement> ret = new TreeSet<Reimbursement>();
+
+		try {
+			ret = dao.filterByGreaterThanDoubleField("AMOUNT", amt);
+		} catch (SQLSecurityException e) {
+			devlog.trace(this, e);
+		}
+		return ret;
+	}
+
+	public Set<Reimbursement> amountLessThan(double amt) {
+		Set<Reimbursement> ret = new TreeSet<Reimbursement>();
+
+		try {
+			ret = dao.filterByLessThanDoubleField("AMOUNT", amt);
+		} catch (SQLSecurityException e) {
+			devlog.trace(this, e);
+		}
+		return ret;
+	}
+
+	public Set<Reimbursement> amountExactly(double amt) {
+		Set<Reimbursement> ret = new TreeSet<Reimbursement>();
+
+		try {
+			ret = dao.filterByExactDoubleField("AMOUNT", amt);
+		} catch (SQLSecurityException e) {
+			devlog.trace(this, e);
+		}
+		return ret;
+	}
+
+	public Set<Reimbursement> findByUser(int id) {
+		Set<Reimbursement> ret = new TreeSet<Reimbursement>();
+
+		try {
+			ret = dao.filterByIntField("AUTHOR", id);
+		} catch (SQLSecurityException e) {
+			devlog.trace(this, e);
+		}
+		return ret;
+	}
+
+	public Set<Reimbursement> findByUser(User u) {
+		Set<Reimbursement> ret = new TreeSet<Reimbursement>();
+
+		try {
+			ret = dao.filterByIntField("AUTHOR", u.getUSER_ID());
+		} catch (SQLSecurityException e) {
+			devlog.trace(this, e);
+		} catch (NullPointerException e) {
+			devlog.trace(this, e);
+		}
+		return ret;
+	}
+
+	public Set<Reimbursement> findByFirstName(String fn) {
+		Set<Reimbursement> ret = new TreeSet<Reimbursement>();
+		if (fn == null) {
+			return ret;
+		} else if (fn.length() > 0 ) {
+			try {
+				ret = dao.filterByStringField("FIRST_NAME", fn);
+			} catch (SQLSecurityException e) {
+				devlog.trace(this,e);
+			}
+		}
 	
+		return ret;
+	}
+
+	public Set<Reimbursement> findByLastName(String ln) {
+		Set<Reimbursement> ret = new TreeSet<Reimbursement>();
+		if (ln == null) {
+			return ret;
+		} else if (ln.length() > 0) {
+			try {
+				ret = dao.filterByStringField("LAST_NAME", ln);
+			} catch (SQLSecurityException e) {
+				devlog.trace(this, e);
+			}
+		}
+		return ret;
+	}
+
+	public Set<Reimbursement> findBy(String col_name, String s) {
+		Set<Reimbursement> ret = new TreeSet<Reimbursement>();
+		if (col_name == null  || s == null) {
+			return ret;
+		} else if (s.length() > 0 && col_name.length() > 1) {
+			try {
+				ret = dao.filterByStringField(col_name, s);
+			} catch (SQLSecurityException e) {
+				devlog.trace(this, e);
+			}
+		}
+		return ret;
+	}
+
 }
--- src/test/java/com/revature/services/FMServicesTest.java
+++ src/test/java/com/revature/services/FMServicesTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 
+import java.util.Set;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -14,10 +16,14 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import com.revature.DAO.ReimbDAO;
+import com.revature.DAO.TestData;
+import com.revature.data.Reimbursement;
 
 public class FMServicesTest {
 	
+	private TestData td = new TestData();
 	private FMServices fm;
+	Set<Reimbursement> r;
 	
 	@Mock
 	private ReimbDAO dao;
@@ -29,16 +35,21 @@ public class FMServicesTest {
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+		TestData.resetDB();
 	}
 
 	@Before
 	public void setUp() throws Exception {
-		MockitoAnnotations.initMocks(this);
+		TestData.resetDB();
+
+		dao = new ReimbDAO("public", "TEST IP");
+		fm = new FMServices(td.fm.getUSER_ID(), dao);
 		
-		when(dao.processReimbursement(111, true)).thenReturn(true);
-		when(dao.processReimbursement(222, false)).thenReturn(true);		
+		dao.createUser(td.employee);
+		dao.createUser(td.fm);
 		
-		fm = new FMServices(dao);
+		dao.createReimbursement(td.r1);
+		dao.createReimbursement(td.r2);
 	}
 
 	@After
@@ -47,17 +58,122 @@ public class FMServicesTest {
 
 	@Test
 	public void testApprove() {
-		assertTrue(fm.approveReimb(111));
+		assertTrue(fm.approveReimb(1));
+		assertTrue(dao.getReimbursement(1).getSTATUS_ID() == 1);
+	}
+	
+	@Test
+	public void testApproveNotPending() {
+		TestData.resetDB();
+
+		dao = new ReimbDAO("public", "TEST IP");
+		fm = new FMServices(td.fm.getUSER_ID(), dao);
+		
+		dao.createUser(td.employee);
+		dao.createUser(td.fm);
+		
+		dao.createReimbursement(td.r1);		
+		td.r2.setSTATUS_ID(-1);
+		dao.createReimbursement(td.r2);
+		assertTrue(!fm.approveReimb(2));
 	}
 	
 	@Test
 	public void testDeny() {
-		assertTrue(fm.denyReimb(222));
+		assertTrue(fm.denyReimb(1));
 	}
 	
 	@Test
-	public void testFilter() {
-		
+	public void testDenyNotPending() {
+		assertTrue(!fm.denyReimb(2));
+	}
+	
+	@Test
+	public void testFilterByStatus() {
+		dao.createReimbursement(td.r2);
+		r = fm.findByStatus(0);
+		assertTrue(r.size() == 1);
+		r = fm.findByStatus(1);
+		assertTrue(r.size() == 1);
+
+	}
+	
+	@Test
+	public void testFilterByStatusNotExist() {
+		TestData.resetDB();
+		assertTrue(fm.findByStatus(0).size() == 0);
+	}
+	
+	@Test
+	public void testGetAllReimbursements() {
+		r = fm.getAllReimbursements();
+		assertTrue(r.size() == 2);
+		assertTrue(r.contains(td.r1));
+		assertTrue(r.contains(td.r2));
+	}
+	
+	@Test
+	public void testGetAllReimbursementsEmpty() {
+		TestData.resetDB();
+		assertTrue(fm.getAllReimbursements().size() == 0);
 	}
 
+	@Test
+	public void testAmountGreaterThan() {
+		r= fm.amountGreaterThan(20);
+		assertTrue(r.size() == 1);
+		assertTrue(r.contains(td.r2));
+	}
+	
+	@Test
+	public void testAmountLessThan() {
+		r= fm.amountLessThan(20);
+		assertTrue(r.size() == 1);
+		assertTrue(r.contains(td.r1));
+	}
+	
+	@Test
+	public void testAmountExactly() {
+		r = fm.amountExactly(100.99);
+		assertTrue(r.size() == 1);
+		assertTrue(r.iterator().next().equals(td.r2));
+	}
+	
+	@Test
+	public void testFindByUser() {
+		r = fm.findByUser(1);
+		assertTrue(r.size() == 2);
+		
+		r = fm.findByUser(-100);
+		assertTrue(r.size() == 0);
+		
+		r = fm.findByUser(null);
+		assertTrue(r.size() == 0);
+	}
+	
+	@Test
+	public void testFindByFirstName() {
+		r = fm.findByFirstName("JOHN");
+		assertTrue(r.size() == 2);
+		r = fm.findByFirstName("JOE");
+		assertTrue(r.size() == 0);
+		r = fm.findByFirstName("");
+		assertTrue(r.size() == 0);
+		r = fm.findByFirstName(null);
+		assertTrue(r.size() == 0);
+	}
+	
+	@Test
+	public void testFindByLastName() {
+		r = fm.findByLastName("DOE");
+		assertTrue(r.size() == 2);
+		r = fm.findByLastName("SMITH");
+		assertTrue(r.size() == 0);
+		r = fm.findByLastName("");
+		assertTrue(r.size() == 0);
+		r = fm.findByLastName(null);
+		assertTrue(r.size() == 0);
+	}
+
+	
 }
